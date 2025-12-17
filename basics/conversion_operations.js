d9306cef4e6b3ec888e9a4e8894277bb21ce42f0
CHANDRIMA SAHA <chandrimasaha68@gmail.com>
2025-12-17 18:37:59 +0000

    Operations added

--- basics/conversion_operations.js
+++ basics/conversion_operations.js
@@ -15,11 +15,53 @@ let score = "false"
 // true => 1 ; false = 0
 
 let value_in_boolean = Boolean(score)
-console.log(typeof value_in_boolean)
-console.log(value_in_boolean)
+// console.log(typeof value_in_boolean)
+// console.log(value_in_boolean)
 
 // if any string is passed to Boolean() -->
 // then if the string has some characters then it will produce --> true
 // else if the string is empty then it will produce --> false
 // e.g. --> "" --> false ; "false" --> true
 // 1 --> True ; 0 --> False
+
+// ------- < Operations > -------
+
+// Basic operations -->
+
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let x = 2
+let negative = -x
+// console.log(negative)
+
+let str1 = "Hello "
+let str2 = "Everyone!"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) // 12
+// console.log("1" + "2") // 12
+// console.log(1 + "2") // 12
+// console.log(1 + "2" + 2) // 122
+// console.log(1 + 2 + "2" + 2) // 322
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") // 32
+
+// console.log(true) // true
+// console.log(+true) // converts true to number --> true => 1
+// console.log(+"") // false => 0
+
+// Increment Operator
+
+let a = 100
+let pre_inc = ++a
+let post_inc = a++
+console.log(++a) // 103
+console.log(a++) // 103
+console.log(pre_inc) // 101
+console.log(post_inc) // 101
