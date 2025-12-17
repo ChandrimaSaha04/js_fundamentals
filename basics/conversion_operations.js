067e63ceda4fa099b4f79127b7866c38275b505f
CHANDRIMA SAHA <chandrimasaha68@gmail.com>
2025-12-17 17:45:22 +0000

    Conversion of datatypes

---
+++ basics/conversion_operations.js
@@ -0,0 +1,25 @@
+let score = "false"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let value_in_number = Number(score)
+// console.log(typeof value_in_number)
+// console.log(value_in_number)
+
+// "33" => 33
+// "33abc" => NaN (Not a number)
+// "abcpw" => Nan
+// null => 0
+// undefined => NaN
+// true => 1 ; false = 0
+
+let value_in_boolean = Boolean(score)
+console.log(typeof value_in_boolean)
+console.log(value_in_boolean)
+
+// if any string is passed to Boolean() -->
+// then if the string has some characters then it will produce --> true
+// else if the string is empty then it will produce --> false
+// e.g. --> "" --> false ; "false" --> true
+// 1 --> True ; 0 --> False
